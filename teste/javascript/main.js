9b9dba1787d3679583cefdf03ab2eb2d53c0fa85
Ravscado <raviaugustobm09@gmail.com>
2026-05-16 11:54:08 -0300

    Add files via upload

---
+++ teste/javascript/main.js
@@ -0,0 +1,12 @@
+let name = prompt("Digite seu nome")
+
+let idade = prompt("Digite sua idade")
+idadevalor = parseFloat(idade)
+
+
+if (idadevalor >= 18) {
+    alert("Já pode tirar a habilitação!")
+} else{
+        alert("Não pode tirar habilitação!")
+}
+    
